511bc6729b0d6c485bfed0c8d13cf75cfa933f30
IanWearHat <ianbbdai@gmail.com>
2020-05-18 22:51:06 -0700

    Added generic trigger prefab
    Generic trigger prefab created to change rooms when hit, based on the id given to it (id being the scene number under "Build Settings"). Can be used for checkpoints or something.

---
+++ RPG-Project-Unity/Assets/Scripts/RoomTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomTrigger : MonoBehaviour
+{
+  public int id; //id should be given through the unity editor
+
+  void OnTriggerEnter2D(Collider2D col) {
+    SceneManager.LoadScene(id);
+  }
+
+}
