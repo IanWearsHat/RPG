cdbdc74934a640123ebed3017b758c9bf170ed8c
IanWearHat <ianbbdai@gmail.com>
2020-05-20 12:16:33 -0700

    Added dialogue ui but no functinoality yet

---
+++ RPG-Project-Unity/Assets/Scripts/DialogueManagerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManagerScript : MonoBehaviour
+{
+
+    public string[] sentences;
+    bool go = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (go) {
+        foreach(string output in sentences) {
+          Debug.Log(output);
+        }
+        go = false;
+      }
+
+    }
+}
